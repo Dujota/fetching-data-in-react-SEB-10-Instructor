ac2c366176e5a761ca86d33470cf2d1dfddc7304
Denis S Dujota <denisdujota@gmail.com>
2024-08-07 15:11:43 +0300

    add the weather on click

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+// Services
+import * as weatherService from './services/weatherService';
+
+// Components
+import WeatherSearch from './components/WeatherSearch/WeatherSearch';
+// src/App.jsx
+import WeatherDetails from './components/WeatherDetails/WeatherDetails';
+
+
+
+const App = () => {
+
+  const [weather, setWeather] = useState({})
+
+
+  const getWeather = async (city) => {
+    const data = await weatherService.show(city);
+
+    const newWeatherState = {
+      location: data.location.name,
+      temperature: data.current.temp_c,
+      condition: data.current.condition.text,
+    };
+
+    setWeather(newWeatherState);
+  }
+
+  console.log(weather)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main>
+      <h1>Hello world!</h1>
+      <WeatherSearch getWeather={getWeather}/>
+      <WeatherDetails weather={weather} />
+    </main>
+  );
 }
 
 export default App
